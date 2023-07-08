527bc842ff4e0e4cf33b67f3c470a9f804aef38e
Dharmik <dharmik_bhandari@hotmail.com>
2023-07-08 12:48:48 +0000

    Added login componenet

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
+import { Validators, FormGroup, FormArray, FormControl, FormBuilder } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+}
