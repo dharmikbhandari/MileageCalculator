be351827af03bc1c25171733379387c3a1efa117
Dharmik <dharmik_bhandari@hotmail.com>
2023-07-14 19:22:03 +0530

    Setting up framework

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,7 +3,6 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { UserService } from 'src/app/shared/services/user.service';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,20 +11,16 @@ import { UserService } from 'src/app/shared/services/user.service';
 export class LoginComponent {
   userServices = inject(UserService);
   authService = inject(AuthService);
-  
 
-  // onSubmit() {
-  //   let pin = this.loginForm.get('pin')?.value;
-  //   if (pin?.toString().length === 4) {
-  //     console.log(this.loginForm.value);
-  //     this.userServices.getUsers().subscribe((data) => {
-  //       const currentUser = data.filter((x) => x.pin == pin);
-  //       if (currentUser.length > 0) {
-  //         this.router.navigate(['/dashboard']);
-  //       } else {
-  //         console.error('Not valid');
-  //       }
-  //     });
-  //   }
-  // }
+  loginForm = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl(),
+  });
+
+  onSubmit() {
+    this.authService.signIn(
+      this.loginForm.get('email')?.value,
+      this.loginForm.get('password')?.value
+    );
+  }
 }
